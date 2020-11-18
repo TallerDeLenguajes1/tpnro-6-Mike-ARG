878294890f62fe22279c16f991c1db972681589f
Miguel <milito_vanetta@hotmail.com>
2020-11-18 04:44:56 -0300

    Haciendo la parte de "Cliente".

---
+++ CadeteriaMVC/CadeteriaMVC/Controllers/ClientesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CadeteriaMVC.Models;
+using System.Data.SQLite;
+
+namespace CadeteriaMVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ILogger<ClientesController> _logger;
+        private List<CadeteriaMVC.Entidades.Cliente> ListaClientes;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+            ListaClientes = new List<CadeteriaMVC.Entidades.Cliente>();
+        }
+
+        public IActionResult Index()
+        {
+            RepositorioCliente R = new RepositorioCliente();
+            ListaClientes = R.GetAll();
+            return View(ListaClientes);
+        }
+
+        public IActionResult AltaCliente()
+        {
+            return View(new CadeteriaMVC.Entidades.Cliente("","",1234));
+        }
+
+        [HttpPost]
+        public IActionResult CrearCliente(CadeteriaMVC.Entidades.Cliente C)
+        {
+            string mensaje;
+            if (ModelState.IsValid)
+            {
+                RepositorioCliente R = new RepositorioCliente();
+                R.Alta(C);
+                mensaje = "Se creo con exito el cliente";
+            }
+            else
+            {
+                mensaje = "Hubo un error, intente de nuevo";
+            }
+
+            return Content(mensaje);
+        }
+
+        public IActionResult UpdateCliente(int id)
+        {
+            RepositorioCliente R = new RepositorioCliente();
+            CadeteriaMVC.Entidades.Cliente C = R.Buscar(id);
+            if (C != null)
+            {
+
+                return View(C);
+            }
+            else
+            {
+                return Content("No se encontró ningun usuario con ese id");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ModificarCliente(CadeteriaMVC.Entidades.Cliente C)
+        {
+            string mensaje;
+            if (ModelState.IsValid)
+            {
+                RepositorioCliente R = new RepositorioCliente();
+                R.Update(C);
+                mensaje = "Se modificó con exito el cliente";
+            }
+            else
+            {
+                mensaje = "Hubo un error, intente de nuevo";
+            }
+
+            return Content(mensaje);
+        }
+
+        public IActionResult BajaCliente(int id)
+        {
+            RepositorioCliente R = new RepositorioCliente();
+            CadeteriaMVC.Entidades.Cliente C = R.Buscar(id);
+            if (C != null)
+            {
+                return View(C);
+            }
+            else
+            {
+                return Content("No se encontró ningun usuario con ese id");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EliminarCliente(CadeteriaMVC.Entidades.Cliente C)
+        {
+            string mensaje;
+            try
+            {
+                RepositorioCliente R = new RepositorioCliente();
+                R.Baja(C.id);
+                mensaje = "Se elimino con exito el cliente";
+            }
+            catch (Exception)
+            {
+                mensaje = "Hubo un error, intente de nuevo";
+            }
+
+            return Content(mensaje);
+        }
+    }
+}
---
+++ CadeteriaMVC/CadeteriaMVC/Models/RepositorioCliente.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace CadeteriaMVC.Models
+{
+    public class RepositorioCliente
+    {
+        public List<CadeteriaMVC.Entidades.Cliente> GetAll()
+        {
+            List<CadeteriaMVC.Entidades.Cliente> ListaClientes = new List<CadeteriaMVC.Entidades.Cliente>();
+
+            string path = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\cadeteria.db");
+            var conection = new SQLiteConnection(path);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            string instruccion = "select * from clientes";
+            command.CommandText = instruccion;
+
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                CadeteriaMVC.Entidades.Cliente C = new CadeteriaMVC.Entidades.Cliente("","",1234);
+
+                C.id = Convert.ToInt32(reader["idCliente"]);
+                C.nombre = reader["nombre"].ToString();
+                C.direccion = reader["direccion"].ToString();
+                C.telefono = (long)Convert.ToDouble(reader["telefono"]);
+                ListaClientes.Add(C);
+            }
+
+            conection.Close();
+            return ListaClientes;
+        }
+
+        public void Alta(CadeteriaMVC.Entidades.Cliente C)
+        {
+            string path = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conection = new SQLiteConnection(path);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            string instruccion = "insert into clientes(nombre, direccion, telefono) values (@nombre, @direccion, @telefono);";
+            command.CommandText = instruccion;
+            command.Parameters.AddWithValue("@nombre", C.nombre);
+            command.Parameters.AddWithValue("@direccion", C.direccion);
+            command.Parameters.AddWithValue("@telefono", C.telefono);
+            command.ExecuteNonQuery();
+
+            conection.Close();
+        }
+
+        public void Update(CadeteriaMVC.Entidades.Cliente C)
+        {
+            string path = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conection = new SQLiteConnection(path);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            string instruccion = "update clientes SET nombre = @nombre, direccion = @direccion, telefono = @telefono where idCliente = @id;";
+            command.CommandText = instruccion;
+            command.Parameters.AddWithValue("@id", C.id);
+            command.Parameters.AddWithValue("@nombre", C.nombre);
+            command.Parameters.AddWithValue("@direccion", C.direccion);
+            command.Parameters.AddWithValue("@telefono", C.telefono);
+            command.ExecuteNonQuery();
+
+            conection.Close();
+        }
+
+        public void Baja(int id)
+        {
+            string path = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conection = new SQLiteConnection(path);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            string instruccion = "delete from clientes where idCliente = @id;";
+            command.CommandText = instruccion;
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+
+            conection.Close();
+        }
+
+        public CadeteriaMVC.Entidades.Cliente Buscar(int id)
+        {
+            CadeteriaMVC.Entidades.Cliente C = new CadeteriaMVC.Entidades.Cliente("", "", 1234);
+
+            string path = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conection = new SQLiteConnection(path);
+            conection.Open();
+
+            var command = conection.CreateCommand();
+            string instruccion = "select * from clientes where idCliente = @id";
+            command.CommandText = instruccion;
+            command.Parameters.AddWithValue("@id", id);
+
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                C.id = Convert.ToInt32(reader["idCliente"]);
+                C.nombre = reader["nombre"].ToString();
+                C.direccion = reader["direccion"].ToString();
+                C.telefono = (long)Convert.ToDouble(reader["telefono"]);
+            }
+
+            conection.Close();
+            return C;
+        }
+
+    }
+}
