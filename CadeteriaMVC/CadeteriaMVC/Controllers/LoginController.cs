e2deafe823fb163c7cbeb42df2748d83db8637ec
Miguel <milito_vanetta@hotmail.com>
2020-12-21 14:07:13 -0300

    Terminado.

--- CadeteriaMVC/CadeteriaMVC/Controllers/LoginController.cs
+++ CadeteriaMVC/CadeteriaMVC/Controllers/LoginController.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using CadeteriaMVC.Entidades;
+using CadeteriaMVC.ViewModel;
+using CadeteriaMVC.Models;
 
 namespace CadeteriaMVC.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ILogger<LoginController> _logger;
+        private readonly IMapper _mapper;
+
+        public LoginController(ILogger<LoginController> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
